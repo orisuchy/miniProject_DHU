a4e7a033cbc8caba7754298688139d93391d31b1
Ori Suchy <orisuchy@gmail.com>
2021-12-15 14:32:00 +0200

    convertToDict

--- tryStanza.py
+++ tryStanza.py
@@ -3,5 +3,8 @@ import stanza
 stanza.download('he')
 nlp = stanza.Pipeline('he', processors='tokenize, pos')
 doc = nlp("אני כל כך עצוב לי וגשם על העיר ודיזינגוף נראה לי כמו רכבת לילה לקהיר")
-doc.sentences[0].print_dependencies()
-print(doc.sentences[0])
+# doc.sentences[0].print_dependencies()
+print(doc)
+
+dicts = doc.to_dict()
+print(dicts)
