dc5e214deb5e5039e8d8244f42b80af83ebf804c
Ori Suchy <orisuchy@gmail.com>
2022-01-25 18:47:55 +0200

    organized code

--- CSVReader.py
+++ CSVReader.py
@@ -1,31 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv("annotations\CatmaTry.csv", sep=';', header=None, usecols=[2, 4, 10], names=["Story", "Sentence", "Type"])
+
+df = pd.read_csv("annotations\CatmaTry.csv", sep=';', header=None, usecols=[2, 4, 10],
+                 names=["Story", "Sentence", "Type"])
 df['Type'] = df['Type'].str.replace('/', '')
 
-descriptive_vect = []
-Notdescriptive_vect = []
-Mightdescriptive_vect = []
-
-for t in df['Type']:
-    if t == 'Descriptive':
-        descriptive_vect.append(1)
-        Notdescriptive_vect.append(0)
-        Mightdescriptive_vect.append(0)
-    elif t == 'NotDescriptive':
-        descriptive_vect.append(0)
-        Notdescriptive_vect.append(1)
-        Mightdescriptive_vect.append(0)
-    elif t == 'MightDescriptive':
-        descriptive_vect.append(0)
-        Notdescriptive_vect.append(0)
-        Mightdescriptive_vect.append(1)
-
+# Init dict with 0's
 new_data = {"Sentence": df['Sentence'],
-            "Descriptive": descriptive_vect,
-            "NotDescriptive": Notdescriptive_vect,
-            "MightDescriptive": Mightdescriptive_vect}
+            "Descriptive": [0] * len(df),
+            "NotDescriptive": [0] * len(df),
+            "MightDescriptive": [0] * len(df)}
+
+# set 1 according to type
+for i, t in enumerate(df['Type']):
+    new_data[t][i] = 1
 
 new_df = pd.DataFrame(new_data)
-
 new_df.to_csv("goodCSV.csv")
